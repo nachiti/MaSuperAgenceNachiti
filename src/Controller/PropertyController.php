dec8b21085f249bf6a6d967e5c29488d525fc2df
nachiti <nachiti3@gmail.com>
2020-01-23 13:47:42 +0100

    Fisrt commit

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Controller;
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class PropertyController extends AbstractController
+{
+
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(PropertyRepository $repository, EntityManagerInterface  $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/biens", name="property.index")
+     * @return Response
+     */
+    public function index(): Response {
+
+        return $this->render('property/index.html.twig',[
+            'current_menu' => 'properties'
+        ]);
+
+    }
+
+}
